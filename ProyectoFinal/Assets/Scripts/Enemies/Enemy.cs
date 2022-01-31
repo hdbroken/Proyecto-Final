8774e2718f7a9b8886d2628d8a32bf6015fa3ef7
hdbroken <hdbroken@gmail.com>
2022-01-30 22:30:10 -0300

    Movimiento automatico enemigo
    Movimiento del enemigo asignando N puntos en el escenario.

--- ProyectoFinal/Assets/Scripts/Enemies/Enemy.cs
+++ ProyectoFinal/Assets/Scripts/Enemies/Enemy.cs
@@ -10,17 +10,40 @@ public class Enemy : MonoBehaviour
 
     private NavMeshAgent _navigation;
 
+    private int _index = 0;
+
+    private void Awake()
+    {
+        _navigation = GetComponent<NavMeshAgent>();        
+    }
     // Start is called before the first frame update
     void Start()
     {
-        _navigation = GetComponent<NavMeshAgent>();
+        _navigation.SetDestination(_waypointList.waypoint[_index].transform.position);
     }
 
     // Update is called once per frame
     void Update()
     {
-        _navigation.SetDestination(_waypointList.waypoint[0].transform.position);
-        _navigation.SetDestination(_waypointList.waypoint[1].transform.position);
-        _navigation.SetDestination(_waypointList.waypoint[2].transform.position);
+        if (_index < _waypointList.waypoint.Count)
+        {
+            if (_navigation.remainingDistance == 0)
+            {
+                if ((_index + 1 < _waypointList.waypoint.Count))
+                {
+                    _index++;
+                    _navigation.SetDestination(_waypointList.waypoint[_index].transform.position);
+                }
+                else
+                {
+                    _index = -1;
+                }
+            }
+
+        }
+        else
+        {
+            _index = -1;
+        }
     }
 }
