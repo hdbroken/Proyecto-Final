e970f0e95ef46284fbf668f5fe361ae12549ede4
SoloWolf <danielaguerreberry@live.com.ar>
2022-02-26 02:31:47 -0300

    Laser 0.1

--- ProyectoFinal/Assets/Scripts/borrar/PlayerBorrar.cs
+++ ProyectoFinal/Assets/Scripts/borrar/PlayerBorrar.cs
@@ -4,40 +4,78 @@ using UnityEngine;
 
 public class PlayerBorrar : MonoBehaviour
 {
-
-
-    [SerializeField] Vector3 Rotacion = new Vector3(0f, 180f, 0f);
-    [SerializeField] Vector3 Position = new Vector3(0f, 0f, 0f);
-    [SerializeField] float Speed = 5f;
-     public Vector3 direccion;
+    [SerializeField] Vector3 StartingRotation = new Vector3(0f, 180f, 0f);
+    [SerializeField] Vector3 StartingPosition = new Vector3(0f, 0f, 0f);
+    [SerializeField] float PlayerSpeed = 5f;
+    //[SerializeField] float sensibility = 1f;
+    private LineRenderer laser;
+    private float cameraX;
+    private float cameraY;
+    private Vector3 direccion;
 
     void Start()
     {
         StartPos();
+        laser = GetComponent<LineRenderer>();
     }
-
     void Update()
     {
         Move();
+        Rotate();
+        Shoot();
     }
 
     void StartPos()
     {
-        transform.Rotate(Rotacion);
-        transform.localPosition = Position;
+        transform.Rotate(StartingRotation);
+        transform.localPosition = StartingPosition;
     }
-
-    void Move() 
+    void Move()
     {
-         direccion = Vector3.zero;
+        direccion = Vector3.zero;
         if (Input.GetKey("w")) { direccion = Vector3.forward; };
         if (Input.GetKey("a")) { direccion = Vector3.left; };
         if (Input.GetKey("s")) { direccion = Vector3.back; };
         if (Input.GetKey("d")) { direccion = Vector3.right; };
-        Debug.Log(transform.position);
-        
-        transform.position = transform.position + direccion * Time.deltaTime * (Speed * 3);
-        
+
+        transform.Translate(direccion * PlayerSpeed * Time.deltaTime);
+    }
+    void Rotate()
+    {
+        cameraY += Input.GetAxis("Mouse X");
+        cameraX += (Input.GetAxis("Mouse Y") * -1);
+        Quaternion angulo = Quaternion.Euler(cameraX, cameraY, 0f);
+        transform.localRotation = angulo;
     }
 
+    void Shoot()
+    {
+        if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.Mouse0))
+        {
+            Debug.Log("piw piu");
+            laser.SetPosition(0, transform.position);
+            laser.SetPosition(1, transform.forward * 1000);
+            RaycastHit hit;
+
+            if (Physics.Raycast(transform.position, transform.forward*1000, out hit))
+            {
+                if (hit.collider == null)
+                {
+                    laser.SetPosition(1, hit.point);
+                }
+            }
+            else
+            {
+                laser.SetPosition(1, transform.forward * 1000);
+            }
+
+        }
+        else
+        {
+            Debug.Log("piw piu");
+            laser.SetPosition(0, transform.position);
+            laser.SetPosition(0, transform.forward);
+
+        }
+    }
 }
