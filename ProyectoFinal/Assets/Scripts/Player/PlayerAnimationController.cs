a4d6683886180d47cdd49f4af4b9b1a2e888f1d6
hdbroken <hdbroken@gmail.com>
2022-03-07 18:13:50 -0300

    Finalizacion Animacion Player

--- ProyectoFinal/Assets/Scripts/Player/PlayerAnimationController.cs
+++ ProyectoFinal/Assets/Scripts/Player/PlayerAnimationController.cs
@@ -19,22 +19,84 @@ public class PlayerAnimationController : MonoBehaviour
         bool IsRun = Input.GetKey(KeyCode.LeftShift);
         bool IsLStrafe = Input.GetKey(KeyCode.A);
         bool IsRStrafe = Input.GetKey(KeyCode.D);
-
+        bool IsGunsighting = Input.GetKey(KeyCode.Mouse1);
+        Debug.Log(IsGunsighting);
         // IDLE
-        _animator.SetBool("isWalk", false);
-        _animator.SetBool("isRun", false);
-        _animator.SetBool("isLStrafe", false);
-        _animator.SetBool("isRStrafe", false);
-        _animator.SetBool("isWalkingBack", false);
-        _animator.SetBool("isRStrafeRun", false);
-        _animator.SetBool("isLStrafeRun", false);
+        /*  _animator.SetBool("isWalk", false);
+          _animator.SetBool("isRun", false);
+          _animator.SetBool("isLStrafe", false);
+          _animator.SetBool("isRStrafe", false);
+          _animator.SetBool("isWalkingBack", false);
+          _animator.SetBool("isRStrafeRun", false);
+          _animator.SetBool("isLStrafeRun", false);*/
         //
-        if (!IsRun)
+        if(IsGunsighting)
+        {
+            _animator.SetBool("isPistolIdle", true);
+
+            _animator.SetBool("isWalk", false);
+            _animator.SetBool("isRun", false);
+            _animator.SetBool("isLStrafe", false);
+            _animator.SetBool("isRStrafe", false);
+            _animator.SetBool("isWalkingBack", false);
+            _animator.SetBool("isRunBack", false);
+            _animator.SetBool("isRStrafeRun", false);
+            _animator.SetBool("isLStrafeRun", false);
+            _animator.SetBool("isPistolStrafeL", false);
+            _animator.SetBool("isPistolStrafeR", false);
+            _animator.SetBool("isPistolWalk", false);
+            _animator.SetBool("isPistolWalkBack", false);
+            
+            if (!IsLStrafe && !IsRStrafe)
+            {
+                //Forward Walk
+                if (IsWalk)
+                {
+                    _animator.SetBool("isPistolWalk", true);
+                }
+                //Back Walk
+                if (IsWalkingBack)
+                {
+                    _animator.SetBool("isPistolWalkBack", true);
+                }
+            }
+            //Strafe
+            if (!IsWalk && !IsWalkingBack)
+            {
+                //LEFT STRAFE
+                if (IsLStrafe)
+                {
+                    _animator.SetBool("isPistolStrafeL", true);
+                }
+                //RIGHT STRAFE
+                if (IsRStrafe)
+                {
+                    _animator.SetBool("isPistolStrafeR", true);
+                }
+            }
+
+            //Strafe + Walk
+            if ((IsLStrafe || IsRStrafe) && IsWalk)
+            {
+                _animator.SetBool("isPistolWalk", true);
+            }
+            if ((IsLStrafe || IsRStrafe) && IsWalkingBack)
+            {
+                _animator.SetBool("isPistolWalkBack", true);
+            }
+
+        }
+        if ((!IsRun)&&(!IsGunsighting))
         {
             _animator.SetBool("isRun", false);
             _animator.SetBool("isRunBack", false);
             _animator.SetBool("isRStrafeRun", false);
             _animator.SetBool("isLStrafeRun", false);
+            _animator.SetBool("isPistolIdle", false);
+            _animator.SetBool("isPistolStrafeL", false);
+            _animator.SetBool("isPistolStrafeR", false);
+            _animator.SetBool("isPistolWalk", false);
+            _animator.SetBool("isPistolWalkBack", false);
             //Walk
             if (!IsLStrafe && !IsRStrafe)
             {
@@ -80,12 +142,17 @@ public class PlayerAnimationController : MonoBehaviour
 
         }
         //Run
-        if (IsRun)
+        if ((IsRun)&& (!IsGunsighting))
         {
-            _animator.SetBool("isRun", true);
+            _animator.SetBool("isRun", false);
             _animator.SetBool("isRunBack", false);
             _animator.SetBool("isRStrafe", false);
             _animator.SetBool("isLStrafe", false);
+            _animator.SetBool("isPistolIdle", false);
+            _animator.SetBool("isPistolStrafeL", false);
+            _animator.SetBool("isPistolStrafeR", false);
+            _animator.SetBool("isPistolWalk", false);
+            _animator.SetBool("isPistolWalkBack", false);
             //Walk
             if (!IsLStrafe && !IsRStrafe)
             {
@@ -94,11 +161,13 @@ public class PlayerAnimationController : MonoBehaviour
                 //Forward Run
                 if (IsWalk)
                 {
+                    _animator.SetBool("isRun", true);
                     _animator.SetBool("isWalk", true);
                 }
                 //Back Run
                 if (IsWalkingBack)
                 {
+                    _animator.SetBool("isRun", true);
                     _animator.SetBool("isWalkingBack", true);
                     _animator.SetBool("isRunBack", true);
                 }
@@ -111,12 +180,14 @@ public class PlayerAnimationController : MonoBehaviour
                 //LEFT STRAFE RUN
                 if (IsLStrafe)
                 {
+                    _animator.SetBool("isRun", true);
                     _animator.SetBool("isLStrafe", true);
                     _animator.SetBool("isLStrafeRun", true);
                 }
                 //RIGHT STRAFE RUN
                 if (IsRStrafe)
                 {
+                    _animator.SetBool("isRun", true);
                     _animator.SetBool("isRStrafe", true);
                     _animator.SetBool("isRStrafeRun", true);
                 }
@@ -125,10 +196,14 @@ public class PlayerAnimationController : MonoBehaviour
             //Strafe + Run
             if ((IsWalk || IsWalkingBack) && IsLStrafe)
             {
+                _animator.SetBool("isRun", true);
+                _animator.SetBool("isLStrafe", true);
                 _animator.SetBool("isLStrafeRun", true);
             }
             if ((IsWalk || IsWalkingBack) && IsRStrafe)
             {
+                _animator.SetBool("isRun", true);
+                _animator.SetBool("isRStrafe", true);
                 _animator.SetBool("isRStrafeRun", true);
             }
 
