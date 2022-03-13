b5d502e3d07bb16adb5483b175580b99e2e3ab5c
SoloWolf <danielaguerreberry@live.com.ar>
2022-03-12 22:30:29 -0300

    3

--- ProyectoFinal/Assets/Scripts/Enemies/PatrolBehavior.cs
+++ ProyectoFinal/Assets/Scripts/Enemies/PatrolBehavior.cs
@@ -4,60 +4,60 @@ using UnityEngine;
 
 public class PatrolBehavior : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
-    [SerializeField] float timeToTurn = 10;
     [SerializeField] float rotationSpeed = 10;
-    float timer;
-    bool timeUp;
-    //deben tomar una unica vez los datos por cada rotacion de 90 grados
-    //https://docs.unity3d.com/ScriptReference/Mathf.LerpAngle.html
     Vector3 actualY;
     Vector3 newY;
-    bool newData = false; 
-
-        Vector3 lol;
-
+    bool newData = false;
+    bool fix = false;
     void Update()
     {
-        timer = timer + Time.deltaTime;
         Turn90();
-        //Move();
     }
-
     void Turn90()
     {
-        if (newData == false)
+        if (transform.eulerAngles == new Vector3(0, 270, 0))
         {
-            Debug.Log("solo debo ver este cartel una vez");
-            actualY = transform.eulerAngles;
-            newY = actualY + new Vector3(0, 90, 0);
-            newData = true;
+            if (newData == false)
+            {
+                actualY = transform.eulerAngles;
+                newY = new Vector3(0, 0, 0);
+                newData = true;
+            }
+            if (transform.eulerAngles.y >= newY.y)
+            {
+                transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime), Space.Self);
+            }
+            /*if (transform.eulerAngles.y < newY.y)
+            {
+                fix = true;
+            }
+            if (fix)
+            {
+                transform.eulerAngles = newY;
+                fix = false;
+            }*/
         }
-        Debug.Log("esta es la posicion inicial y no tiene que cambiar" + actualY);
-        Debug.Log("esta es la posicion final y no tiene que cambiar" + newY);
-
-        if (lol != newY)
+        else
         {
-            Debug.Log(" transform es " +transform.eulerAngles);
-            transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
-
-            Vector3 lol = new Vector3(Mathf.Round(transform.eulerAngles.x), Mathf.Round(transform.eulerAngles.y),Mathf.Round(transform.eulerAngles.z) );
-
-            Debug.Log("lol es " +lol);
+            if (newData == false)
+            {
+                actualY = transform.eulerAngles;
+                newY = actualY + new Vector3(0, 90, 0);
+                newData = true;
+            }
+            if (transform.eulerAngles.y <= newY.y)
+            {
+                transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime), Space.Self);
+            }
+            if (transform.eulerAngles.y > newY.y)
+            {
+                fix = true;
+            }
+            if (fix)
+            {
+                transform.eulerAngles = newY;
+                fix = false;
+            }
         }
-    } 
-
-    /*void Move()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
-        
-        if(timeToTurn > timer) 
-        {
-            transform.Rotate(Vector3.up * (rotationSpeed * Time.deltaTime));
-        }
-    }*/
-
-
-
-
+    }
 }
