734625d86dcd401ce2e95359eb78b083211f276f
SoloWolf <danielaguerreberry@live.com.ar>
2022-03-27 20:12:50 -0300

    27/03

--- ProyectoFinal/Assets/Scripts/Enemies/PatrolShoot.cs
+++ ProyectoFinal/Assets/Scripts/Enemies/PatrolShoot.cs
@@ -4,12 +4,39 @@ using UnityEngine;
 
 public class PatrolShoot : MonoBehaviour
 {
-
     [SerializeField] float speed = 5;
+    [SerializeField] float disappear = 10;
+    private float timer;
+    private float xyz=1;
+
     void Update()
     {
+        timer += Time.deltaTime;
         transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        Remove();
+        Debug.Log(timer);
     }
+
+    void Remove() 
+    {
+        if (timer>=(disappear-2))
+        {
+            transform.localScale= new Vector3(xyz,xyz,xyz);
+            xyz-=(0.5f*Time.deltaTime);            
+        }
+
+        if (timer>=disappear)
+        {
+            transform.localScale= new Vector3(timer,timer,timer);
+            xyz=0;
+            timer=0;
+            Destroy(gameObject);
+        }
+    }
+
+
+
+
 
 }
 
